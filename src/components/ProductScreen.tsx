bd3c5a07d91f506abf13556369f69b05557cfdb6
emmamariagaby <emmamariagaby@gmail.com>
2020-03-20 10:15:50 +0100

    created components StartScreen ProductScreen CheckoutScreen

---
+++ src/components/ProductScreen.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+/**
+ * Product page with dog and cat food
+ */
+
+ class ProductScreen extends React.Component {
+     render() {
+         return (
+             <></>
+         )
+     }
+ }
+
+ export default ProductScreen;
