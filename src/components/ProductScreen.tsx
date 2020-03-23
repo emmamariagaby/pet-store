97e6a735f3987f3f93189aa68f9d93ec3aae1d6d
Embla <emblajonsson@gmail.com>
2020-03-23 19:23:19 +0100

    added boxes

--- src/components/ProductScreen.tsx
+++ src/components/ProductScreen.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Grommet, Grid, Box, Button } from 'grommet'
+import PetFood from './PetFood';
 
 /**
  * Product page with dog and cat food
  */
 
- class ProductScreen extends React.Component {
-     render() {
-         return (
-             <></>
-         )
-     }
- }
+class ProductScreen extends React.Component {
+    render() {
+        return (
+            <PetFood />
+        )
+    }
+}
 
- export default ProductScreen;
+export default ProductScreen;
--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Grommet, Button, Header, Menu, Main, Paragraph, Box, Image} from 'grommet';
+import { Grommet, Button, Header, Menu, Main, Paragraph, Box, Image } from 'grommet';
 import { Basket } from 'grommet-icons';
+import ProductScreen from './ProductScreen';
 
 
 /**
@@ -11,24 +12,25 @@ class StartScreen extends React.Component {
   render() {
     return (
       <Grommet theme={header}>
-      <div className="StartScreen">
-      <Header background="brand">
-      <Menu label="SHOP" items={[{ label: 'Petfood'}]} />
-  <h1>pet store</h1>
-  <Button icon={<Basket />} hoverIndicator />
-</Header>
-<Main pad="large">
-  <h2>Petfood since 1999</h2>
-  <Paragraph>Pet Store is a online store with high quality petfood for cats and dogs</Paragraph>
-</Main>
-<Box height="large" width="large">
-  <Image 
-  fit="cover"
-  src="petstore.jpg"
-  />
-</Box>
+        <div className="StartScreen">
+          <Header background="brand">
+            <Menu label="SHOP" items={[{ label: 'Petfood' }]} />
+            <h1>pet store</h1>
+            <Button icon={<Basket />} hoverIndicator />
+          </Header>
+          <Main pad="large">
+            <h2>Petfood since 1999</h2>
+            <Paragraph>Pet Store is a online store with high quality petfood for cats and dogs</Paragraph>
+          </Main>
+          <Box height="large" width="large">
+            <Image
+              fit="cover"
+              src="petstore.jpg"
+            />
+          </Box>
+          <ProductScreen />
         </div>
-        </Grommet>
+      </Grommet>
     )
   }
 }
