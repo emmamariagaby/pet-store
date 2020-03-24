f9258f1e4ecc8179f3b3dd4a3713eff154dccc7b
Louise Bäckström <backstromlouise@gmail.com>
2020-03-25 00:09:56 +0100

    buttons for counting items with onClick in Props

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,12 +8,14 @@ export interface Food {
     type: string
     animal: string
     img: string
+    count: number
   }
 export type Cart = {
     id: number
     type: string
     animal: string
     img: string
+    count: number
   }
   
   interface Props {}
@@ -21,7 +23,7 @@ export type Cart = {
   interface State {
     dcfood: Food[]
     cart: Cart[]
-  
+    count: number
   }
 
 export default class App extends React.Component<Props, State> {
@@ -32,24 +34,29 @@ export default class App extends React.Component<Props, State> {
             id: 0,
             type: 'dry',
             animal: 'dog',
-            img: 'https://cdn.royalcanin-weshare-online.io/XiKc-mQBaxEApS7LrwSZ/v2/mini-dental-ccn-packshot?w=320&auto=compress&fm=jpg'
+            img: 'https://cdn.royalcanin-weshare-online.io/XiKc-mQBaxEApS7LrwSZ/v2/mini-dental-ccn-packshot?w=320&auto=compress&fm=jpg',
+            count: 0
           }, {
             id: 1,
             type: 'wet',
             animal: 'dog',
-            img: 'https://cdn.royalcanin-weshare-online.io/zCJQa2sBaxEApS7LRh5U/v80/ad-jack-russel-packshot-bhn18?w=320&auto=compress&fm=jpg'
+            img: 'https://cdn.royalcanin-weshare-online.io/zCJQa2sBaxEApS7LRh5U/v80/ad-jack-russel-packshot-bhn18?w=320&auto=compress&fm=jpg',
+            count: 0
           }, {
             id: 2,
             type: 'dry',
             animal: 'cat',
-            img: 'https://cdn.royalcanin-weshare-online.io/m2kUrmsBG95Xk-RB3Pu-/v4/hairskin-ne-fcn-packshot?w=320&auto=compress&fm=jpg'
+            img: 'https://cdn.royalcanin-weshare-online.io/m2kUrmsBG95Xk-RB3Pu-/v4/hairskin-ne-fcn-packshot?w=320&auto=compress&fm=jpg',
+            count: 0
           }, {
             id: 3,
             type: 'wet',
             animal: 'cat',
-            img: 'https://cdn.royalcanin-weshare-online.io/-Gkua2QBG95Xk-RBidKW/v2/16-kitten-sterilised-b1-ne?w=320&auto=compress&fm=jpg'
+            img: 'https://cdn.royalcanin-weshare-online.io/-Gkua2QBG95Xk-RBidKW/v2/16-kitten-sterilised-b1-ne?w=320&auto=compress&fm=jpg',
+            count: 0
         }],
-            cart: []
+            cart: [],
+            count: 0
         }
       }
 
@@ -69,12 +76,28 @@ export default class App extends React.Component<Props, State> {
         })
       }
 
+      removeOne = (food: Food) => {
+        this.setState({
+          cart: [
+            ...this.state.food.count( + 1)
+          ]
+        })
+      }
+
+      addOne = (food: Food) => {
+        this.setState({
+          count: (this.state.count - 1)
+        })
+      }
+    
+       
+
     render() {
     return (
         <div className="App">
             <StartScreen />
             <ProductScreen dcfood={this.state.dcfood} cart={this.state.cart} addFood={this.addFood}/>
-            <CheckoutScreen handleRemove={this.handleRemove} cart={this.state.cart}/>
+            <CheckoutScreen addOne={this.addOne} removeOne={this.removeOne} handleRemove={this.handleRemove} cart={this.state.cart} food={this.state.cart}/>
         </div>
         )
     }
