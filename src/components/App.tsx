4b65400bc98264407d137404cf9d8a22a76712b1
Louise Bäckström <backstromlouise@gmail.com>
2020-03-25 16:33:47 +0100

    added quantity to cart

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -12,14 +12,14 @@ export interface Food {
     type: string
     animal: string
     img: string
-    count: number
+    quantity: number
   }
 export type Cart = {
     id: number
     type: string
     animal: string
     img: string
-    count: number
+    quantity: number
   }
   
   interface Props {}
@@ -27,7 +27,8 @@ export type Cart = {
   interface State {
     dcfood: Food[]
     cart: Cart[]
-    count: number
+    quantity: number
+    food: Food[]
   }
 
 export default class App extends React.Component<Props, State> {
@@ -39,62 +40,50 @@ export default class App extends React.Component<Props, State> {
             type: 'dry',
             animal: 'dog',
             img: 'https://cdn.royalcanin-weshare-online.io/XiKc-mQBaxEApS7LrwSZ/v2/mini-dental-ccn-packshot?w=320&auto=compress&fm=jpg',
-            count: 0
+            quantity: 1
           }, {
             id: 1,
             type: 'wet',
             animal: 'dog',
             img: 'https://cdn.royalcanin-weshare-online.io/zCJQa2sBaxEApS7LRh5U/v80/ad-jack-russel-packshot-bhn18?w=320&auto=compress&fm=jpg',
-            count: 0
+            quantity: 1
           }, {
             id: 2,
             type: 'dry',
             animal: 'cat',
             img: 'https://cdn.royalcanin-weshare-online.io/m2kUrmsBG95Xk-RB3Pu-/v4/hairskin-ne-fcn-packshot?w=320&auto=compress&fm=jpg',
-            count: 0
+            quantity: 1
           }, {
             id: 3,
             type: 'wet',
             animal: 'cat',
             img: 'https://cdn.royalcanin-weshare-online.io/-Gkua2QBG95Xk-RBidKW/v2/16-kitten-sterilised-b1-ne?w=320&auto=compress&fm=jpg',
-            count: 0
+            quantity: 1
         }],
             cart: [],
-            count: 0
+            quantity: 1,
+            food: []
         }
       }
 
       addFood = (food: Food)=>{
         this.setState({
-            cart: [...this.state.cart, food]
+          cart: [...this.state.cart, food]
           })
       }
 
       handleRemove = (food: Food) => {
         let index = this.state.cart.indexOf(food);
+    
         this.setState({
+          quantity: this.state.quantity - 1,
           cart: [
             ...this.state.cart.slice(0, index),
-            ...this.state.cart.slice(index + 1)
+            ...this.state.cart.slice(index + 1),
           ]
         })
       }
-
-      removeOne = (food: Food) => {
-        this.setState({
-          cart: [
-            ...this.state.food.count( + 1)
-          ]
-        })
-      }
-
-      addOne = (food: Food) => {
-        this.setState({
-          count: (this.state.count - 1)
-        })
-      }
-    
-       
+      addOne(){}
 
     render() {
     return (
@@ -112,13 +101,20 @@ export default class App extends React.Component<Props, State> {
             </Route>
             <Route path="/ProductScreen" component={ProductScreen}>
             <ProductScreen dcfood={this.state.dcfood} cart={this.state.cart} addFood={this.addFood}/>
+<<<<<<< HEAD
             </Route>
             <Route path="/CheckoutScreen" component={CheckoutScreen}>
             <CheckoutScreen handleRemove={this.handleRemove} cart={this.state.cart}/>
             </Route>
        </Switch>
+=======
+            {this.state.cart.map(item => (
+            <CheckoutScreen food={item} handleRemove={this.handleRemove} cart={this.state.cart} addOne={this.addOne}/>
+            ))}
+>>>>>>> added quantity to cart
         </div>
         </Router>
         )
-    }
+  }
+
 }
