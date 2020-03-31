e5a1dc4189ce1807af221bb5e331d188f9d447f7
Embla <emblajonsson@gmail.com>
2020-03-31 16:03:39 +0200

    moved component to petfood

--- src/components/PetFood/PetFood.tsx
+++ src/components/PetFood/PetFood.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Food } from '../App'
-import { Grommet, Button, Header, Menu, Main, Paragraph, Box, Image, Grid} from 'grommet';
+import { Grommet, Button, Header, Menu, Main, Paragraph, Box, Image, Grid } from 'grommet';
 import './PetFood.css'
+import Modal from '../Modal';
 
 
 interface Props {
     food: Food
     addToCart: (food: Food) => void
-    
+
 }
 
 interface State {
-  message: string
+    message: string
 }
 
 export default class PetFood extends React.Component<Props, State> {
@@ -19,15 +20,15 @@ export default class PetFood extends React.Component<Props, State> {
         super(props)
         this.state = {
             message: ' '
-            }
         }
-    handleClick(){
-        this.props.addToCart({id: this.props.food.id,type: this.props.food.type, animal: this.props.food.animal, img: this.props.food.img, price: this.props.food.price, total: this.props.food.total, quantity: this.props.food.quantity})
-        
-        this.setState({ 
-           message: 'Added To Cart'
+    }
+    handleClick() {
+        this.props.addToCart({ id: this.props.food.id, type: this.props.food.type, animal: this.props.food.animal, img: this.props.food.img, price: this.props.food.price, total: this.props.food.total, quantity: this.props.food.quantity })
+
+        this.setState({
+            message: 'Added To Cart'
         })
-      }
+    }
 
     render() {
         return (
@@ -36,15 +37,16 @@ export default class PetFood extends React.Component<Props, State> {
                     width="medium" height="medium" direction="row"
                     pad="small">
                     <Box justify="between" align="center"
-                        width="medium"height="medium" border={{ color: '#DADADA', size: 'small' }}
+                        width="medium" height="medium" border={{ color: '#DADADA', size: 'small' }}
                         pad="small" margin="small">
-                        <img src={this.props.food.img}/>
+                        <img src={this.props.food.img} />
                         <li>{this.props.food.type + ' '} {this.props.food.animal}</li>
                         <li>{this.props.food.price + ' kr'}</li>
+                        <Modal />
                         <Button
                             label="Add"
                             onClick={() => this.handleClick()} primary
-                            />{this.state.message}
+                        />{this.state.message}
                     </Box>
                 </Box>
             </Grommet>
