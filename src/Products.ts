65e96598554da5c94985ac578bc7ce156dcc0adc
Louise Bäckström <backstromlouise@gmail.com>
2020-04-01 17:24:53 +0200

    added products in a separate file

---
+++ src/Products.ts
@@ -0,0 +1,105 @@
+import React from 'react'
+import App from "./components/App"
+import CheckoutScreen from "./components/CheckoutScreen"
+import { Food } from "./components/App"
+
+  
+  export const data: Food[] = [
+           {
+            id: 0,
+            type: 'Dry, Mini Adult',
+            animal: 'Dog',
+            img: 'https://cdn.royalcanin-weshare-online.io/v2k6a2QBG95Xk-RBN9oV/v256/packshot-mini-ad-shn17?w=320&auto=compress&fm=jpg',
+            price: 250, 
+            total: 0,
+            quantity: 0
+            }, {
+            id: 1,
+            type: 'Dry, Medium Adult',
+            animal: 'Dog',
+            img: 'https://cdn.royalcanin-weshare-online.io/5yLrrWsBaxEApS7LIx-P/v26/packshot-med-ad7-shn17?w=250',
+            price: 250,
+            total: 0,
+            quantity: 0
+            }, {
+            id: 2,
+            type: 'Dry',
+            animal: 'Dog, Giant Adult',
+            img: 'https://cdn.royalcanin-weshare-online.io/evo5a2QBIYfdNSoCWQT-/v20/packshot-giant-ad-shn17?w=320&auto=compress&fm=jpg',
+            price: 300,
+            total: 0,
+            quantity: 0
+            }, { 
+            id: 3,
+            type: 'Wet, Mini Adult',
+            animal: 'Dog',
+            img: 'https://cdn.royalcanin-weshare-online.io/_CIQrmsBaxEApS7L9R-E/v2/shn18-mini-adult-s-pouch-85?w=320&auto=compress&fm=jpg',
+            price: 300,
+            total: 0,
+            quantity: 0
+            }, {
+            id: 4,
+            type: 'Wet, Medium Adult',
+            animal: 'Dog',
+            img: 'https://cdn.royalcanin-weshare-online.io/-yIPrmsBaxEApS7L3x89/v32/shn18-medium-adult-s-pouch-140?w=320&auto=compress&fm=jpg',
+            price: 250, 
+            total: 0,
+            quantity: 0
+            }, {
+            id: 5,
+            type: 'Wet, Maxi Adult',
+            animal: 'Dog',
+            img: 'https://cdn.royalcanin-weshare-online.io/kmkOrmsBG95Xk-RB0ftX/v30/shn18-maxi-adult-s-pouch-140?w=250',
+            price: 250,
+            total: 0,
+            quantity: 0
+            }, {
+            id: 6,
+            type: 'Dry, Hairball Care',
+            animal: 'Cat',
+            img: 'https://cdn.royalcanin-weshare-online.io/yCEya2QBaxEApS7LaP13/v34/hairball-s-fcn-packshot?w=320&auto=compress&fm=jpg',
+            price: 250, 
+            total: 0,
+            quantity: 0
+            }, {
+            id: 7,
+            type: 'Dry, Indoor',
+            animal: 'Cat',
+            img: 'https://cdn.royalcanin-weshare-online.io/9Gkua2QBG95Xk-RBh9KI/v22/16-indoor-27-b1-ne?w=320&auto=compress&fm=jpg',
+            price: 250,
+            total: 0,
+            quantity: 0
+            }, {
+            id: 8,
+            type: 'Dry',
+            animal: 'Cat, Outdoor',
+            img: 'https://cdn.royalcanin-weshare-online.io/QyEua2QBaxEApS7LkPuL/v14/16-outdoor-b1-ne?w=320&auto=compress&fm=jpg',
+            price: 300,
+            total: 0,
+            quantity: 0
+            }, { 
+            id: 9,
+            type: 'Wet, Hairball Care',
+            animal: 'Cat',
+            img: 'https://cdn.royalcanin-weshare-online.io/w_oya2QBIYfdNSoCRQBd/v16/fcnw16-hairball-cig-s-pouch?w=320&auto=compress&fm=jpg',
+            price: 300,
+            total: 0,
+            quantity: 0
+            }, {
+            id: 10,
+            type: 'Wet, Steriliserad Jelly',
+            animal: 'Cat',
+            img: 'https://cdn.royalcanin-weshare-online.io/Ivoxa2QBIYfdNSoCLwBY/v12/fhnw16-ster-cij-s-pouch?w=320&auto=compress&fm=jpg',
+            price: 250, 
+            total: 0,
+            quantity: 0
+            }, {
+            id: 11,
+            type: 'Wet, Intense Beauty',
+            animal: 'Cat',
+            img: 'https://cdn.royalcanin-weshare-online.io/uCEya2QBaxEApS7LS_35/v44/fcnw16-int-beauty-cig-s-pouch?w=320&auto=compress&fm=jpg',
+            price: 250,
+            total: 0,
+            quantity: 0
+            }]
+           
--- src/components/ProductScreen.tsx
+++ src/components/ProductScreen.tsx
@@ -12,17 +12,25 @@ import { Link } from 'react-router-dom';
 
   
   interface Props {
-    dcfood: Food[]
+    dogCatFood: Food[]
     cart: Cart[]
     addFood: (food: Food) => void
   }
 
   interface State {
-   
+    
   
   }
 
  class ProductScreen extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+        
+    }
+}
+
+
       render() {
         return (
             <Grommet theme={header}>
@@ -39,12 +47,17 @@ import { Link } from 'react-router-dom';
                 <Main pad="large" justify="center" align="center">
                     <h2>ONLINE SHOP</h2>
                     <h3>Add petfood to your cart</h3>
-                    <List items={this.props.dcfood} addFood={this.props.addFood}/>
+  
+                    <List items={this.props.dogCatFood} addFood={this.props.addFood}/>
+                 
                     <ul className="nav-links">
                       <Link to='/CheckoutScreen'>
                         <li>Go to cart</li>
                       </Link>
                     </ul>
+                   
+                   
+                 
                 </Main>
                 <Footer background="#DADADA" pad="small">
                     <h5>Created by<br></br>emmamariagaby emmbla louisebackstrom @ github</h5>
