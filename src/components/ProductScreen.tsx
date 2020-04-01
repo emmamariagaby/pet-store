c7c3f12f8bb021c9a3963b609d891582896b2765
Embla <emblajonsson@gmail.com>
2020-04-01 11:03:01 +0200

    button

--- src/components/ProductScreen.tsx
+++ src/components/ProductScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grommet, Button, Header, Menu, Main, Paragraph, Box, Image, Footer, Anchor, Grid} from 'grommet';
+import { Grommet, Button, Header, Menu, Main, Paragraph, Box, Image, Footer, Anchor, Grid } from 'grommet';
 import List from './List';
 import { Basket, Home } from 'grommet-icons';
 import { Cart } from './App'
@@ -10,62 +10,61 @@ import { Link } from 'react-router-dom';
  * Product page with dog and cat food
  */
 
-  
-  interface Props {
-    dcfood: Food[]
-    cart: Cart[]
-    addFood: (food: Food) => void
+
+interface Props {
+  dcfood: Food[]
+  cart: Cart[]
+  addFood: (food: Food) => void
+}
+
+interface State {
+
+
+}
+
+class ProductScreen extends React.Component<Props, State> {
+  render() {
+    return (
+      <Grommet theme={header}>
+        <Header background="brand" pad="large">
+          <Menu
+            label='Menu'
+            items={[
+              { label: 'HOME', href: '/' }
+            ]}
+          />
+          <h1>pet store</h1>
+          <Link to='/CheckoutScreen'>
+            <Button icon={<Basket />} />
+          </Link>
+        </Header>
+        <Main pad="large" justify="center" align="center">
+          <h2>ONLINE SHOP</h2>
+          <h3>Add petfood to your cart</h3>
+          <List items={this.props.dcfood} addFood={this.props.addFood} />
+          <ul className="nav-links">
+          </ul>
+        </Main>
+        <Footer background="#DADADA" pad="small">
+          <h5>Created by<br></br>emmamariagaby emmbla louisebackstrom @ github</h5>
+          <Anchor href="InformationScreen" label="INFORMATION" />
+        </Footer>
+      </Grommet>
+    )
   }
+}
 
-  interface State {
-   
-  
-  }
-
- class ProductScreen extends React.Component<Props, State> {
-      render() {
-        return (
-            <Grommet theme={header}>
-                <Header background="brand" pad="large">
-                <Menu
-                label='Menu'
-                items={[
-                  { label: 'HOME', href: '/' }
-           ]}
-    />
-                    <h1>pet store</h1>
-                    <Button href="CheckoutScreen" icon={<Basket />} />
-                </Header>
-                <Main pad="large" justify="center" align="center">
-                    <h2>ONLINE SHOP</h2>
-                    <h3>Add petfood to your cart</h3>
-                    <List items={this.props.dcfood} addFood={this.props.addFood}/>
-                    <ul className="nav-links">
-                      <Link to='/CheckoutScreen'>
-                        <li>Go to cart</li>
-                      </Link>
-                    </ul>
-                </Main>
-                <Footer background="#DADADA" pad="small">
-                    <h5>Created by<br></br>emmamariagaby emmbla louisebackstrom @ github</h5>
-                    <Anchor href="InformationScreen" label="INFORMATION"/>
-                </Footer>       
-            </Grommet>
-        )
-      }
-    }
-
-    const header = {
-      global: {
-        font: {
-          family: 'Roboto',
-          size: '15px',
-          color: "brand",
-          height: '20px',
-          justify: 'center',
-          align: 'center'
-        },
-      },
-    };
+const header = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '15px',
+      color: "brand",
+      height: '20px',
+      justify: 'center',
+      align: 'center'
+    },
+  },
+};
 
 export default ProductScreen;
