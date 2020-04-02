60a302eef0f431964abfdb418e8a20da9634fd95
Louise Bäckström <backstromlouise@gmail.com>
2020-04-02 14:35:41 +0200

    added info about the products

--- src/Products.ts
+++ src/Products.ts
@@ -1,6 +1,3 @@
-import React from 'react'
-import App from "./components/App"
-import CheckoutScreen from "./components/CheckoutScreen"
 import { Food } from "./components/App"
 
   
@@ -12,7 +9,8 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/v2k6a2QBG95Xk-RBN9oV/v256/packshot-mini-ad-shn17?w=320&auto=compress&fm=jpg',
             price: 250, 
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Dog Food - Adult dogs of small adult breeds (up to 10 kg) - Older than 10 months.'
             }, {
             id: 1,
             type: 'Dry, Medium Adult',
@@ -20,7 +18,8 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/5yLrrWsBaxEApS7LIx-P/v26/packshot-med-ad7-shn17?w=250',
             price: 250,
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Dog Food - Adult dogs of medium breed (from 11 to 25 kg) - From 12 months to 7 years of age.'
             }, {
             id: 2,
             type: 'Dry',
@@ -28,7 +27,8 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/evo5a2QBIYfdNSoCWQT-/v20/packshot-giant-ad-shn17?w=320&auto=compress&fm=jpg',
             price: 300,
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Dog Food - For adult dogs very large breeds (> 45 kg) - Over 18/24 months.'
             }, { 
             id: 3,
             type: 'Wet, Mini Adult',
@@ -36,7 +36,8 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/_CIQrmsBaxEApS7L9R-E/v2/shn18-mini-adult-s-pouch-85?w=320&auto=compress&fm=jpg',
             price: 300,
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Dog Food - For adult small breeds (between 1 and 10 kg) - From 10 months to 12 years.'
             }, {
             id: 4,
             type: 'Wet, Medium Adult',
@@ -44,7 +45,8 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/-yIPrmsBaxEApS7L3x89/v32/shn18-medium-adult-s-pouch-140?w=320&auto=compress&fm=jpg',
             price: 250, 
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Dog Food - For adult dogs medium sized breeds (between 11 and 25 kg) - From 12 months to 10 years.'
             }, {
             id: 5,
             type: 'Wet, Maxi Adult',
@@ -52,7 +54,8 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/kmkOrmsBG95Xk-RB0ftX/v30/shn18-maxi-adult-s-pouch-140?w=250',
             price: 250,
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Dog food - For adult dogs large adult breeds (between 26 and 44 kg) - From 15 months to 8 years.'
             }, {
             id: 6,
             type: 'Dry, Hairball Care',
@@ -60,7 +63,8 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/yCEya2QBaxEApS7LaP13/v34/hairball-s-fcn-packshot?w=320&auto=compress&fm=jpg',
             price: 250, 
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Cat Food - Adult Cats - Recommended to support skin and fur health.'
             }, {
             id: 7,
             type: 'Dry, Indoor',
@@ -68,7 +72,8 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/9Gkua2QBG95Xk-RBh9KI/v22/16-indoor-27-b1-ne?w=320&auto=compress&fm=jpg',
             price: 250,
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Cat food - Balanced feed for older indoor cats (7-12 years).'
             }, {
             id: 8,
             type: 'Dry',
@@ -76,7 +81,8 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/QyEua2QBaxEApS7LkPuL/v14/16-outdoor-b1-ne?w=320&auto=compress&fm=jpg',
             price: 300,
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Cat food - Balanced feed for adult cats (1-7 years), who often stay outdoors and are active.'
             }, { 
             id: 9,
             type: 'Wet, Hairball Care',
@@ -84,7 +90,8 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/w_oya2QBIYfdNSoCRQBd/v16/fcnw16-hairball-cig-s-pouch?w=320&auto=compress&fm=jpg',
             price: 300,
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Cat food - Feeding for adult cats (thin slices in sauce).'
             }, {
             id: 10,
             type: 'Wet, Steriliserad Jelly',
@@ -92,7 +99,8 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/Ivoxa2QBIYfdNSoCLwBY/v12/fhnw16-ster-cij-s-pouch?w=320&auto=compress&fm=jpg',
             price: 250, 
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Cat Food - Feed for neutered adult cats (thin slices of jelly).'
             }, {
             id: 11,
             type: 'Wet, Intense Beauty',
@@ -100,6 +108,7 @@ import { Food } from "./components/App"
             img: 'https://cdn.royalcanin-weshare-online.io/uCEya2QBaxEApS7LS_35/v44/fcnw16-int-beauty-cig-s-pouch?w=320&auto=compress&fm=jpg',
             price: 250,
             total: 0,
-            quantity: 0
+            quantity: 0,
+            info: 'Cat food - Feeding for adult cats (thin slices in jelly).'
             }]
            
