dc7d2c95712efd7170be228fd82b87a372ce4278
Louise Bäckström <backstromlouise@gmail.com>
2020-04-03 11:55:51 +0200

    added modalCheckout

---
+++ src/CompleteOrder.ts
@@ -0,0 +1,6 @@
+import { Complete } from "./components/Payment/ModalCheckout"
+
+export const complete: Complete = {
+    
+        info: 'Tack för din order ditt order nummer är '
+}
---
+++ src/components/Payment/ModalCheckout.tsx
@@ -0,0 +1,91 @@
+import React, { Component } from 'react'
+import { Grommet, Button, Box, grommet, Layer } from 'grommet'
+import { Link } from 'react-router-dom';
+import  { complete }  from '../../CompleteOrder'
+
+export type Complete = {
+    info: string
+}
+
+export type CompleteOrder = {
+    info: string
+}
+
+interface Props {
+}
+
+interface State {
+    showLayer: boolean
+    text: Complete
+    order: string
+    random: number
+}
+
+
+export default class ModalCheckout extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            showLayer: false,
+            text: complete,
+            order: '',
+            random: Math.floor(Math.random() * 100000)+1000
+        }
+
+        this.completeOrder = this.completeOrder.bind(this);
+        
+       
+    }
+    completeOrder() {
+        this.setState(prevState => ({
+            showLayer: !prevState.showLayer,
+            order: this.state.text.info
+        }));
+    }
+
+    render() {
+       
+        
+        console.log(this.state.text.info)
+
+        return (
+            <Grommet theme={grommet}>
+                <Link
+                  to={{
+                    pathname: "/CheckoutScreen"
+                  }}
+                ><Button
+                label="Complete"
+                onClick={this.completeOrder}
+                primary
+                
+            /></Link>
+                
+                {this.state.showLayer && (
+                    <Layer full animation="fadeIn">
+                        <Box width="large" height="large" align="center" justify="center" fill background="white" pad="medium">
+                        <Box width="large" height="large" align="center" justify="center" pad="medium" border={{ color: '#DADADA', size: 'small' }}>
+                        
+                            <p>{this.state.order + this.state.random}</p>
+                       
+                            <Link
+                            to={{
+                                pathname: "/CheckoutScreen"
+                            }}
+                            >
+                            <Button
+                                primary
+                                label="Close"
+                                onClick={this.completeOrder}
+                            />
+
+                            </Link>
+
+                            </Box> 
+                        </Box>
+                    </Layer>
+                )}
+            </Grommet >
+        )
+    }
+}
